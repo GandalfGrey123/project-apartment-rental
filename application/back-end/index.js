d15bfe704360633ebd0b527bb59e021a98dff28e
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-03-15 12:55:41 -0700

    add body parser to app config

--- application/back-end/index.js
+++ application/back-end/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+var bodyParser = require('body-parser')
 
 const listingRouter = require('./routes/listingrouter');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors());
 
 app.use('/newlisting',listingRouter);
 
