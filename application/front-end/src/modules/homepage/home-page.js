45620efde9ab089c178500e76ac7a774f6a172d0
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-03-20 15:00:03 -0700

    base homepage for prototype

---
+++ application/front-end/src/modules/homepage/home-page.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import HomeGrid from './component/home-grid';
+
+class HomePage extends Component{
+
+	render(){
+	 return(
+	  <HomeGrid />
+	 );
+	}
+
+}
+
+
+export default (HomePage);
+
+
+
