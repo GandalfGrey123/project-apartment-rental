3cd03508c4d5e3c46e20ee7e293a4f24fa9d49d1
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-03-21 22:45:30 -0700

    setup template for routes

--- application/back-end/routes/listingrouter.js
+++ application/back-end/routes/listingrouter.js
@@ -6,12 +6,18 @@ const router = express.Router();
 //permanently save a listing 
 router.post('/', function(req,res){
 	
-	models.listing.create({
-		// add data to database
+	models.ListingPost.create({
 
 	}).then(function(){
 		res.redirect('/');
 	});
 });
+
+
+router.get('/', function(req,res){
+	
+});
+
+
 	
 module.exports = router;
