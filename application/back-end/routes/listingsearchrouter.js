7226a2b20058c3eda514ea3e6ec357fcd9c196b0
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-03-22 20:29:21 -0700

    Update listingsearchrouter.js

--- application/back-end/routes/listingsearchrouter.js
+++ application/back-end/routes/listingsearchrouter.js
@@ -4,6 +4,12 @@ var models = require('../models');
 const router = express.Router();
 
 router.get('/', function(req,res){
+  //models.ListingPost.findAll().then((result) => res.json(result))
+  models.ListingPost.findAll({
+  	include: [models.HousingType,models.ListingImage]
+  }).then(listings =>{
+  	console.log(res.json(listings));
+  });
 	
 });
 
