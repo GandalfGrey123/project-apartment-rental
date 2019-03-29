4c431e6b81bbda21bf66dab65eae1074df81abc7
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-03-28 22:39:26 -0700

    added all route and route for custom query string

--- application/back-end/routes/listing-router.js
+++ application/back-end/routes/listing-router.js
@@ -1,41 +1,72 @@
 const express = require('express');
 var models = require('../models');
 
-const router = express.Router();
+let multer = require('multer');
+let fileUploads = multer();
 
-router.get('/', function(req,res){
+const router = express.Router();
+const Op = require('sequelize').Op;
+
+
+router.get('/all',function(req,res){
   models.ListingPost.findAll({
-  	include: [models.HousingType,models.ListingImage]
+    include: [models.HousingType,models.ListingImage]
   }).then(listings =>{
-  	res.json(listings);
+    res.json(listings);
   });
 });
 
+
+//route for custom HousingTypes serach 
+router.get('/', function(req,res){
+  //req.query.housingTypes => returns array of selected housing types
+  console.log(req.query.housingTypes);
+  
+  //models.HousingType.findAll({
+  //  where: {
+  //    type:[
+  //      {
+  //        [Op.or]: req.query.housingTypes
+  //      }
+  //    ]
+  //  }
+  //	//include: [models.HousingType,models.ListingImage]
+  //}).then(listings =>{
+  //	res.json(listings);
+  //});
+});
+
+
+//return all housing types that website provides
 router.get('/types', (req, res) => {
   models.HousingType
     .findAll()
     .then((types) => res.json(types));
 });
 
-module.exports = router;
 
-/*
+//new listing
+router.post('/new',fileUploads.fields([]),function(req,res){
 
+  models.ListingPost.create({ 
 
-const router = express.Router();
+    title:req.body.title,
+    description:req.body.description,
+    price:req.body.price,
+    address:req.body.address,
+    city:req.body.city,
+    state:req.body.state,
+    zipCode:req.body.zip,
+    bedrooms:req.body.bedrooms,
+    bathrooms:req.body.bathrooms,    
+    isApproved: false,
 
-//permanently save a listing 
-router.post('/', function(req,res){
- 
- models.ListingPost.create({
- 	
+    HousingTypeId: 1,
+    UserId: 2,
+
   }).then(function(){
-	  res.redirect('/');
+    res.redirect('/');
   });
-
 });
 
-	
 module.exports = router;
-
-*/
