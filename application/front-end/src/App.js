48884ca51274542a0e440c4dcf43f2ce347fb2f9
Romeel Chaudhari <rchaudhari@mail.sfsu.edu>
2019-04-13 16:14:34 -0700

    Added login
    added login st

--- application/front-end/src/App.js
+++ application/front-end/src/App.js
@@ -17,6 +17,7 @@ import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import AboutPage from './modules/about/about-page';
 import HomePage from './modules/homepage/home-page';
 import NewListing from './modules/listing/new-listing';
+import LoginForm from './modules/Login/LoginForm';
 
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
@@ -77,6 +78,7 @@ class App extends Component {
               <Route path={'/about'} component={AboutPage} />
               <Route path={'/new'} component={NewListing} />
               <Route path={'/'} component={HomePage} />
+               <Route path={'/'} component={LoginForm} />
             </Switch>
           </div>  
 
