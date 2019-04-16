0e8c3651feb92d0baf744449c4dc3971515e031f
Romeel Chaudhari <rchaudhari@mail.sfsu.edu>
2019-04-16 14:25:27 -0700

    Updated index.js

--- application/front-end/src/index.js
+++ application/front-end/src/index.js
@@ -33,3 +33,9 @@ app.use(express.static('./client/dist/'));
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
+render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('root')
+)
