7dcf348246f6ee76883cf0951187502418566cb4
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-21 23:00:48 -0700

    setup REST for new message and get inbox messages

---
+++ application/back-end/routes/chat-router.js
@@ -0,0 +1,36 @@
+const express = require('express');
+var models = require('../models');
+const router = express.Router();
+const app = express();
+
+router.post('/new', (req,res) =>{
+	models.Message.create({
+		message: req.body.message;
+		senderUserId: req.body.senderId,
+	    recieverUserId: req.body.recieverId,
+	}).then((newMessage) => {
+		res.json(newMessage);
+	});
+});
+
+router.get('/inbox', (req,res) =>{
+	models.Message.findAll({
+		include: models.User,
+		where: {'senderUserId': req.body.id },
+	}).then((result) =>{
+		res.json(result);
+	});
+});
+
+
+// a better get inbox query
+
+//router.get('/getMyInbox',(req,res)=>{
+//	
+//	models.User.findOne({
+//		where:{id: req.body.userId},
+//		include:[ models.Message]
+//	}).then((user) => {
+//		// find all messages from this user
+//	});
+//});
