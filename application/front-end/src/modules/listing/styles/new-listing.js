c353b060240928b54b8aed430594a1b21bfc2966
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-24 18:13:35 -0700

    Update new-listing.js

--- application/front-end/src/modules/listing/styles/new-listing.js
+++ application/front-end/src/modules/listing/styles/new-listing.js
@@ -3,10 +3,36 @@ const styles = theme => ({
       flexGrow: 1,
       padding: 10,
       margin: 20,
+      width: '70%',
+   	  marginLeft: '15%',
   },
   formGroup: {
-    width: 500
-  }
+    width: '100%'
+
+  },
+   card: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+    zIndex:-1,
+  },
+  gridList: {
+    width: '100%',
+    backgroundColor:'grey',
+  },
+   actions: {
+    display: 'flex',
+    zIndex:1000,
+  },
+   icon: {
+    margin: theme.spacing.unit,
+    fontSize: 32,
+    color:'white',
+  },
+ 
+
 });
 
 export default styles;
