811273effd2f06f8429c94b52f973ef4d276d50e
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-24 18:12:30 -0700

    Update App.js

--- application/front-end/src/App.js
+++ application/front-end/src/App.js
@@ -2,82 +2,36 @@ import React, { Component } from 'react';
 //import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import {
-  AppBar, Toolbar, Typography,
-  IconButton,
-} from '@material-ui/core';
-
-import { 
-  Menu as MenuIcon,
-  Info as InfoIcon
-} from '@material-ui/icons';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import MainBar from './modules/mainbar/main-bar';
 
 import AboutPage from './modules/about/about-page';
 import HomePage from './modules/homepage/home-page';
 import NewListing from './modules/listing/new-listing';
-import ContactInbox from './modules/contact/contact-inbox';
-
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
-
+import ContactPage from './modules/contact/contact-page';
 
 const styles = {
   root: {
     flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
   },
 };
 
 class App extends Component {
   render() {
 
-    const { classes } = this.props;
-
+    const { classes} = this.props;    
+  
     return (
       <BrowserRouter>
         <div className={classes.root}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton 
-                className={classes.menuButton} 
-                color="inherit"
-                aria-label="Menu"
-                component={Link}
-                to={'/about'}
-              >
-                <InfoIcon />
-              </IconButton>
-              <Typography variant="h6" color="inherit" className={classes.grow} component={Link} to={'/'} >
-                SFSU - CSC 648 Team #9 Project
-              </Typography>
-
-                <Button 
-                    variant="contained" 
-                    color="primary"
-                    component={Link}
-                    to={'/new'}
-                >
-                   New Listing
-                   <AddIcon />
-                </Button>
-
-
-            </Toolbar>
-          </AppBar>
+          <MainBar />  
 
           <div className={classes.content} >
             <Switch>
               <Route path={'/about'} component={AboutPage} />
               <Route path={'/new'} component={NewListing} />
-              <Route path={'/inbox'} component={ContactInbox} />
+              <Route path={'/inbox'} component={ContactPage} />
               <Route path={'/'} component={HomePage} />
             </Switch>
           </div>  
