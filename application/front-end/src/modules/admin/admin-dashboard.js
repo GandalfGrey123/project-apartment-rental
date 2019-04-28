43bfee58f92b1c14b64510d3f6fca541b137105f
LightProgrammerZM <34073626+LightProgrammerZM@users.noreply.github.com>
2019-04-28 12:06:51 -0700

    Removed the highlighted drawer tags in admin-dashboard files

--- application/front-end/src/modules/admin/admin-dashboard.js
+++ application/front-end/src/modules/admin/admin-dashboard.js
@@ -1,57 +1,145 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from 'react';
+import {
+  Drawer, withStyles, CssBaseline,
+  Divider, Checkbox, Button,
+  List, ListItem, ListItemText, ListSubheader,
+  Grid,
+} from '@material-ui/core';
+import ListingCard from './component/listing-card';
+import styles from './styles/admin-dashboard';
+import _ from 'lodash';
+import { getHouseTypes, getListings } from '../../api/listings.actions';
 
-const styles = {
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-};
-
-function MediaCard(props) {
-  const { classes } = props;
+const FormRow = ({ listings, props }) => {
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <React.Fragment>
+      {
+        listings.map((value) => (
+          <Grid item xs={4}>
+            <ListingCard
+              listing={value}
+            />
+          </Grid>
+        ))
+      }  
+    </React.Fragment>
   );
 }
 
-MediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+class AdminDashboard extends Component {
 
-export default withStyles(styles)(MediaCard);
+  constructor(props) {
+    super(props);
+    this.state = {
+      listings: [],
+      types: ['All'], // All by default, other types will come from DB.
+      selectedTypes: [] // Empty means all
+    };
+    this.isChecked = this.isChecked.bind(this);
+    this.getListings = this.getListings.bind(this);
+    this.getHousingTypes = this.getHousingTypes.bind(this);
+    this.selectHousingType = this.selectHousingType.bind(this);
+  }
+
+  componentWillMount() {
+    this.getHousingTypes();
+    this.getListings();
+  }
+
+  getListings = () => {
+    const { selectedTypes } = this.state;
+    let params = {};
+    if(!_.isEmpty(selectedTypes)){
+      params = new URLSearchParams();
+      selectedTypes.forEach((value) => params.append("type", value));
+    }
+    getListings(params, (data) => {
+      this.setState({ listings: data || [] })
+    })
+  }
+
+  getHousingTypes = () => {
+    let { types } = this.state;
+    getHouseTypes((data) => {
+      types = types.concat(
+        data.map((value) => _.capitalize(value.type))
+      );
+      this.setState({ types: types })
+    })
+  }
+
+  selectHousingType = type => event => {
+    if (type === 'All') {
+      this.setState({ selectedTypes: [] });
+    } else {
+      let { selectedTypes } = this.state;
+      if (event.target.checked) selectedTypes.push(type);
+      else _.remove(selectedTypes, (i) => i === type);
+      this.setState({ selectedTypes: selectedTypes });
+    }
+  };
+
+  isChecked = (text) => {
+    return (text === 'All' && _.isEmpty(this.state.selectedTypes))
+      || this.state.selectedTypes.includes(text);
+  }
+
+  displayListings = (listings) => {
+    let rows = [];
+    for(let i = 0; i < listings.length; i += 3){
+      rows.push(
+        <Grid container item xs={12} spacing={24}>
+          <FormRow
+            listings={listings.slice(i, i + 3)}
+            props={this.props}
+          />
+        </Grid>
+      );
+    }
+    return rows;
+  } 
+
+  render() {
+
+    const classes = this.props.classes;
+    const { types, listings } = this.state;
+
+    return (
+      <div className={classes.root}>
+          <Grid container spacing={8}>
+             <Grid item  xs={3}>
+                 <CssBaseline />      
+                 <Drawer
+                   className={classes.drawer}
+                   variant='permanent'
+                   classes={{ paper: classes.drawerPaper }}
+                   anchor="left"
+                 >
+                 <List subheader={<ListSubheader> Housing Types</ListSubheader>} className={classes.subList}>
+                   {types.map((text, index) => (
+                     <ListItem button key={`item-${index}`}>
+                       <Checkbox
+                         checked={this.isChecked(text)}
+                         onChange={this.selectHousingType(text)}
+                       />
+                       <ListItemText primary={text} />
+                     </ListItem>
+                   ))}
+                   </List>
+                 <Button color="primary" onClick={() => { this.getListings(); }}>
+                   Update
+                 </Button>
+                 <Divider />
+                 </Drawer> 
+              </Grid>
+
+            <Grid item xs={9}>
+               {this.displayListings(listings)}
+            </Grid>
+          </Grid>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(AdminDashboard);
