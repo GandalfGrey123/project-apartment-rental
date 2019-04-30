6ebddc30ea14fc05f1808c520844418c9860597f
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-30 10:05:11 -0700

    Update chat.js

--- application/back-end/models/chat.js
+++ application/back-end/models/chat.js
@@ -13,7 +13,7 @@ const Chat = sequelize.define('Chat', {
     Chat.hasMany(models.Message);
     Chat.hasOne(models.ListingPost);
     Chat.belongsTo(models.User, {foreignKey: 'user1'});    
-    Chat.belongsTo(models.User, {foreignKey: 'user1'});    
+    Chat.belongsTo(models.User, {foreignKey: 'user2'});    
   };
 
  return Chat;
