c04cb8474a508b11736d7b4126b53aa608a8188c
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-05-16 22:15:30 -0700

    Update message.actions.js

--- application/front-end/src/api/message.actions.js
+++ application/front-end/src/api/message.actions.js
@@ -2,23 +2,20 @@ import axios from "axios";
 import api_config from './config/api.config';
 
 
+export const getInbox = (handleResponse) => {
+  let sessionToken = JSON.parse(sessionStorage.getItem('session')).token
 
-//refractor to retrieve chatObject {chatId, listingTitle, landLordEmail}
-
-// export const getInbox = (handleResponse) => {
-//   let sessionToken = JSON.parse(sessionStorage.getItem('session')).token
-
-//    axios({
-//       method: 'get',
-//       url: `http://${api_config.environment}/messages/inbox`,
-//       headers:{
-//       	'Session': sessionToken
-//       },
-//    }).then((res) => {
-//    	 //http response returns array of json chat objects
-//    	 handleResponse(res.data);
-//    });
-// };
+   axios({
+      method: 'get',
+      url: `http://${api_config.environment}/messages/inbox`,
+      headers:{
+      	'Session': sessionToken
+      },
+   }).then((res) => {
+   	 //http response returns array of json chat objects
+   	 handleResponse(res.data);
+   });
+};
 
 
 export const sendMessage = (messagePacket, handleResponse) => {
